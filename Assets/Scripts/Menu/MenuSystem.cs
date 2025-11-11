feeb409b851bf8cd5c32af1d0b9141f4dbe2623e
duranysd <duranysd1119@gmail.com>
2025-11-10 20:05:13 -0600

    Puerta

--- Assets/Scripts/Menu/MenuSystem.cs
+++ Assets/Scripts/Menu/MenuSystem.cs
@@ -4,6 +4,21 @@ using UnityEngine.SceneManagement;
 
 public class MenuSystem : MonoBehaviour
 {
+    public GameObject MenuPrincipal;
+    public GameObject MenuOpciones;
+
+    public void abrirOpciones()
+    {
+        MenuPrincipal.SetActive(true);
+        MenuOpciones.SetActive(false);
+    }
+
+    public void abrirMenu()
+    {
+        MenuPrincipal.SetActive(false);
+        MenuOpciones.SetActive(true);
+    }
+
     public void Start()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
